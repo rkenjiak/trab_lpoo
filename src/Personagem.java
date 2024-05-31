7aa62154b5433e427e09b80f5b326b3bab6d6d70
Rodrigo Kobayashi <kobayashi.kenji.r@gmail.com>
2024-05-30 22:46:58 -0400

    update msg

--- src/Personagem.java
+++ src/Personagem.java
@@ -21,30 +21,33 @@ public class Personagem {
         }
     }
 
-
     // Método de ataque de this(A) contra inimigo(B)
     public void atacar(Personagem inimigo){
         // Verificação se o atacante está vivo
         if(this.estaMorto()){
-            System.out.printf("O %s não consegue atacar, pois está morto.\n", this.nomeTipo);
-        
-        // Verificação se o defensor está vivo
-        }else if(inimigo.estaMorto()){
-            System.out.printf("Pare! O %s já está morto!\n", inimigo.nomeTipo);
+            System.out.printf("O %s não consegue atacar, pois está morto.\n", this.nomeTipo);        
+        }else{
+            System.out.printf("O %s ataca o %s com %s.\n", this.nomeTipo, inimigo.nomeTipo, this.arma.getNome());        
 
-        // Caso ataque bem sucedido
-        }else if(this.forca > inimigo.forca && this.destreza > inimigo.destreza){   
-            inimigo.recebeDano(this.calculaDano());
+            // Verificação se o defensor está vivo
+            if(inimigo.estaMorto()){
+                System.out.printf("Pare! O %s já está morto!\n", inimigo.nomeTipo);
 
-        // Caso ataque mal sucedido
-        }else if(this.forca < inimigo.forca || this.destreza < inimigo.destreza){   
-            this.recebeDano(inimigo.calculaDano());
+            // Caso ataque bem sucedido
+            }else if(this.forca > inimigo.forca && this.destreza > inimigo.destreza){   
+                inimigo.recebeDano(this.calculaDano());
+                System.out.printf("O ataque foi efetivo com %.1f pontos de dano!\n", this.calculaDano());
 
-        // Caso ataque defendido
-        }else if(this.forca == inimigo.forca || this.destreza == inimigo.destreza){ 
-            System.out.printf("O ataque foi defendido, ninguem se machucou!\n");
+            // Caso ataque mal sucedido
+            }else if(this.forca < inimigo.forca || this.destreza < inimigo.destreza){   
+                this.recebeDano(inimigo.calculaDano());
+                System.out.printf("O ataque foi inefetivo e revidado com %.1f pontos de dano!\n", inimigo.calculaDano());
+
+            // Caso ataque defendido
+            }else if(this.forca == inimigo.forca || this.destreza == inimigo.destreza){ 
+                System.out.printf("O ataque foi defendido, ninguem se machucou!\n");
+            }            
         }
-
         // Impressao de Status pós ataque
         this.printStatus();
         inimigo.printStatus();
